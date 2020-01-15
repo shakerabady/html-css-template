a722c1f4555cf75d66900cfc1cff187600c65a42
shakerabady <shakerdrais@gmail.com>
2020-01-15 02:32:32 +0200

    done with the next button

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
 var carouselSlide = document.querySelector(".carousel-slide");
 var carouselImages = document.querySelectorAll(".carousel-slide div");
 var first = document.querySelectorAll(".carousel-slide div")[0]
-console.log(carouselImages);
+// console.log(carouselImages);
 
 var prev = document.querySelector("#prev");
 var next = document.querySelector("#next");
 
 
-// the size of the image (width) + the margin
+
 var size = carouselImages[0].clientWidth + 30;
 
-// the equation for the single item to move on the x-axis
+carouselSlide.style.transform = "translateX(" + -size + "px)";
+var i = 0;
+next.addEventListener("click", () => {
+var alldivs = document.getElementById("cc");
+console.log("all divs: ",alldivs)
+if (i < 7 && i!==0) {
+  i++
+  console.log("i val: ", i)
+}  else {
+  i = 0;
+}
+elem = alldivs.removeChild(alldivs.getElementsByTagName('div')[i]);
+console.log("elem: ",elem)
+ cln = elem.cloneNode(true)
+
+document.getElementById('cc').appendChild(cln)
+
+
+carouselSlide.style.transition = "transform 0.6s ease-in-out";
 carouselSlide.style.transform = "translateX(" + -size + "px)";
 
-// what happens when next is clicked
-next.addEventListener("click", () => {
-  
-  var shaker = document.getElementById("cc");
-  var pc = shaker.removeChild(first)
- var  y = shaker.appendChild.lastChild
- console.log(y)
-  
+});
 
+var j = carouselImages.length;
+console.log(j)
+prev.addEventListener("click", () => {
+  var alldivs = document.getElementById("cc");
+  console.log("all divs: ",alldivs)
+  if (j > 0 ) {
+    j--
+    console.log("i val: ", j)
+  }  else {
+    j = carouselImages;
+  }
+  elem = alldivs.removeChild(alldivs.getElementsByTagName('div')[j]);
+  console.log("elem: ",elem)
+   cln = elem.cloneNode(true)
+  
+  document.getElementById('cc').insertBefore(cln)
+  
+  
   carouselSlide.style.transition = "transform 0.6s ease-in-out";
-  // counter++;
-  // console.log(counter);
   carouselSlide.style.transform = "translateX(" + -size + "px)";
 });
-// what happens when prev function is triggered
-prev.addEventListener("click", () => {
-  if (counter === 1) {
-    counter = images.length-1
+// prev.addEventListener("click", () => {
+//   if (counter === 1) {
+//     counter = images.length-1
     // return;
     // use return to make the slider stops when it reaches the limits
-  }
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  console.log(counter);
-  carouselSlide.style.transform = "translateX(" + -size  + "px)";
-});
+//   }
+//   carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//   counter--;
+//   console.log(counter);
+//   carouselSlide.style.transform = "translateX(" + -size  + "px)";
+// });
 
 // in case wanted the slider to start over once it reaches the limits
 
